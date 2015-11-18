ca2d650920d7eb5ce30ff11dc4e3d659d9740589
marsimunovic <msimunovic@outlook.com>
2015-11-18 16:57:23 +0100

    Started adding Shi Tomasi corner detector code

--- LearnOCV/main.cpp
+++ LearnOCV/main.cpp
@@ -9,6 +9,54 @@
 using namespace cv;
 using namespace std;
 
+int maxCorners = 120;
+int maxTrackbar = 100;
+
+RNG rng(12345);
+char* source_window = "Image";
+
+void goodFeaturesToTrack_Demo(cv::Mat& src)
+{
+  if( maxCorners < 1 ) { maxCorners = 1; }
+
+  /// Parameters for Shi-Tomasi algorithm
+  vector<Point2f> corners;
+  double qualityLevel = 0.01;
+  double minDistance = 10;
+  int blockSize = 3;
+  bool useHarrisDetector = false;
+  double k = 0.04;
+
+  /// Copy the source image
+  Mat copy;
+  copy = src.clone();
+
+  Mat src_gray;
+  cvtColor( src, src_gray, COLOR_BGR2GRAY );
+  /// Apply corner detection
+  goodFeaturesToTrack( src_gray,
+               corners,
+               maxCorners,
+               qualityLevel,
+               minDistance,
+               Mat(),
+               blockSize,
+               useHarrisDetector,
+               k );
+
+
+  /// Draw corners detected
+  cout<<"** Number of corners detected: "<<corners.size()<<endl;
+  int r = 4;
+  for( int i = 0; i < corners.size(); i++ )
+     { circle( copy, corners[i], r, Scalar(rng.uniform(0,255), rng.uniform(0,255),
+              rng.uniform(0,255)), -1, 8, 0 ); }
+
+  /// Show what you got
+  namedWindow( source_window, WINDOW_AUTOSIZE );
+  imshow( source_window, copy );
+}
+
 int readVideo(int argc, char *argv[])
 {
     stringstream conv;
@@ -52,21 +100,28 @@ int readVideo(int argc, char *argv[])
         if(frameNum%4)
             continue;
         cout << "Frame: " << frameNum << "# " << endl;
-
+        goodFeaturesToTrack_Demo(frameReference);
+/*
         Mat dst, cdst;
         Canny(frameReference, dst, 50, 200, 3);
+
         cvtColor(dst, cdst, COLOR_GRAY2BGR);
         vector<Vec4i> lines;
-        HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
-        for( size_t i = 0; i < lines.size(); i++ )
+        HoughLinesP(dst, lines, 1, CV_PI/180,  100, 50, 10 );
+        for( size_t i = 1; i < lines.size(); i++ )
         {
             Vec4i l = lines[i];
+            Vec4i prev_l = lines[i-1];
+            Point a(l[0], l[1]);
+            Point b(prev_l[2], prev_l[3]);
+            double res = cv::norm(a-b);//Euclidian distance
+            cout << i << " : " << res << endl;
             line( frameReference, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
         }
 
-
-        imshow(WIN_RF, frameReference);
-        int delay = 20;
+        imshow(WIN_RF, dst);
+*/
+        int delay = 20000;
         char c = (char)waitKey(delay);
         if(c == 27)
             break;
@@ -77,8 +132,8 @@ int readVideo(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    //readVideo(argc, argv);
-    //return 0;
+    readVideo(argc, argv);
+    return 0;
     string imageName("../data/images/piano_plain1.jpg");
     if(argc > 1)
     {
