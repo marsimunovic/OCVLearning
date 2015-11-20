11b72134b5b022e636e50c8190c3459c3c4acc80
marsimunovic <msimunovic@outlook.com>
2015-11-20 14:30:15 +0100

    Add code for square detection

--- LearnOCV/calibinit.cpp
+++ LearnOCV/calibinit.cpp
@@ -3,11 +3,129 @@
 #include <opencv2/highgui.hpp>
 
 #include <stdarg.h>
-
+#include <vector>
 
 using namespace cv;
+using namespace std;
 
 static int cvCheckKeyboard(cv::Mat &src, cv::Size size);
+
+
+int thresh = 50, N = 11;
+const char* wndname = "Square Detection Demo";
+
+// helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+static double angle( Point pt1, Point pt2, Point pt0 )
+{
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+
+// returns sequence of squares detected on the image.
+// the sequence is stored in the specified memory storage
+static void findSquares( const Mat& image, vector<vector<Point> >& squares )
+{
+    squares.clear();
+
+    Mat pyr, timg, gray0(image.size(), CV_8U), gray;
+
+    // down-scale and upscale the image to filter out the noise
+    pyrDown(image, pyr, Size(image.cols/2, image.rows/2));
+    pyrUp(pyr, timg, image.size());
+    vector<vector<Point> > contours;
+
+    // find squares in every color plane of the image
+    for( int c = 0; c < 1; c++ )
+    {
+        int ch[] = {c, 0};
+        mixChannels(&timg, 1, &gray0, 1, ch, 1);
+
+        // try several threshold levels
+        for( int l = 0; l < N; l++ )
+        {
+            // hack: use Canny instead of zero threshold level.
+            // Canny helps to catch squares with gradient shading
+            if( l == 0 )
+            {
+                // apply Canny. Take the upper threshold from slider
+                // and set the lower to 0 (which forces edges merging)
+                Canny(gray0, gray, 0, thresh, 5);
+                // dilate canny output to remove potential
+                // holes between edge segments
+                dilate(gray, gray, Mat(), Point(-1,-1));
+            }
+            else
+            {
+                // apply threshold if l!=0:
+                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+                gray = gray0 >= (l+1)*255/N;
+            }
+
+            // find contours and store them all as a list
+            findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+
+            vector<Point> approx;
+
+            // test each contour
+            for( size_t i = 0; i < contours.size(); i++ )
+            {
+                // approximate contour with accuracy proportional
+                // to the contour perimeter
+                approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+
+                // square contours should have 4 vertices after approximation
+                // relatively large area (to filter out noisy contours)
+                // and be convex.
+                // Note: absolute value of an area is used because
+                // area may be positive or negative - in accordance with the
+                // contour orientation
+                if( approx.size() == 4 &&
+                    fabs(contourArea(Mat(approx))) > 1000 &&
+                    isContourConvex(Mat(approx)) )
+                {
+                    double maxCosine = 0;
+
+                    for( int j = 2; j < 5; j++ )
+                    {
+                        // find the maximum cosine of the angle between joint edges
+                        double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+                        maxCosine = MAX(maxCosine, cosine);
+                    }
+
+                    // if cosines of all angles are small
+                    // (all angles are ~90 degree) then write quandrange
+                    // vertices to resultant sequence
+                    if( maxCosine < 0.3 )
+                        squares.push_back(approx);
+                }
+            }
+        }
+    }
+}
+
+
+// the function draws all the squares in the image
+static void drawSquares( Mat& image, const vector<vector<Point> >& squares )
+{
+    for( size_t i = 0; i < squares.size(); i++ )
+    {
+        const Point* p = &squares[i][0];
+        int n = (int)squares[i].size();
+        Mat cimage;
+        cvtColor(image, cimage, CV_GRAY2BGR);
+        polylines(cimage, &p, &n, 1, true, Scalar(0,255,0), 3, LINE_AA);
+    }
+    namedWindow(wndname, WINDOW_NORMAL);
+    imshow(wndname, image);
+    waitKey(0);
+}
+
+
 
 //=====================================================================================
 
@@ -118,10 +236,35 @@ int cvCheckKeyboard(cv::Mat &src, cv::Size size)
     {
         cv::threshold(white, thresh, thresh_level + black_white_gap,
                       255, CV_THRESH_BINARY);
+        vector<vector<Point> > squares;
+        findSquares(thresh, squares);
+        drawSquares(thresh, squares);
+        squares.clear();
+
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+
+        findContours(thresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+        cv::threshold(black, thresh, thresh_level, 255, CV_THRESH_BINARY_INV);
+
+        findContours(thresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+        /// Draw contours
+        ///
+        ///
+        ///
+          Mat drawing = Mat::zeros( thresh.size(), CV_8UC3 );
+          RNG rng(12345);
+          for( int i = 0; i< contours.size(); i++ )
+             {
+               Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+               drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+             }
+
         namedWindow("1", WINDOW_NORMAL);
-        imshow("1", thresh);
+        imshow("1", drawing);
         waitKey(0);
     }
+
 
     return 0;
 }
